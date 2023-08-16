6652944d6f3bf2b0bc802141f0f65f6ea6538d41
Ann Bazykina <annzita19@gmail.com>
2023-08-16 14:48:57 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.num;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.num = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.setRange(this.min, this.num);
     }
 
     greater() {
-
+        this.setRange(this.num, this.max);
     }
 }
 
